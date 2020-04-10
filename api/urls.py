56c1983e08792d5a78d659518de56cda9e1065c3
ttran9 <toddtran9@gmail.com>
2020-04-09 20:23:17 -0700

    adding a path to be able to get the token.

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import include
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import CreateView, DetailsView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = {
     path('bucketlists/', CreateView.as_view(), name="create"),
-    path('bucketlists/<int:pk>', DetailsView.as_view(), name="details"),
+    path('bucketlists/<int:pk>/', DetailsView.as_view(), name="details"),
+    path('get-token/', obtain_auth_token)
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)

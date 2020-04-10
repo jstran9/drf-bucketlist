59b36b84b4a5ddae2b3ee724280f923643e9f392
ttran9 <toddtran9@gmail.com>
2020-04-09 20:21:42 -0700

    registering a receiver to generate a token upon user login.

--- api/models.py
+++ api/models.py
@@ -1,6 +1,11 @@
 
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from django.dispatch import receiver
 
 
 class Bucketlist(models.Model):
@@ -8,7 +13,15 @@ class Bucketlist(models.Model):
     name = models.CharField(max_length=255, blank=False, unique=True, default="Title")
     date_created = models.DateTimeField(default=timezone.now)
     date_modified = models.DateTimeField(default=timezone.now)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='bucketlists', on_delete=models.CASCADE, null=True)     
 
     def __str__(self):
         """Return a human readable representation of the model instance."""
         return "{}".format(self.name)
+
+
+# This receiver handles token creation immediately a new user is created.
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
